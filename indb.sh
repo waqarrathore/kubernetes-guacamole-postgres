c6bdbb12c0f837051296b953ebba665991a98493
waqarrathore <waqar.rathore@gmail.com>
2024-02-03 13:53:40 +0000

    first commit

---
+++ indb.sh
@@ -0,0 +1,12 @@
+kubectl config set-context --current --namespace=guacamole
+kubectl apply -f 00-gauca-namespace.yaml
+kubectl apply -f 08-guaca-secret.yaml
+kubectl apply -f 01-guac-pgsql-pv.yaml
+kubectl apply -f 02-guaca-pclam.yaml
+kubectl apply -f 02-pgsql-svc.yaml
+kubectl apply -f 06-pgsql-dep.yaml
+sleep 20
+#kubectl config set-context --current --namespace=guacamole
+POD=$(kubectl get pod -l app=postgres -o jsonpath="{.items[0].metadata.name}")
+kubectl exec -i $POD -- psql -U guacamole -d guacamole_db < initdb.sql
+kubectl apply -f /home/svc_mgmt/kub-gauc/
